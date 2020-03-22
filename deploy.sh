c9fb964b24d577bfb2083e72e7b9e7bac65f9e3f
Boris Tane <boris.tane@gmail.com>
2020-03-22 16:28:05 +0000

    does it

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf layer
+mkdir layer
+pushd layer
+curl -O https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar xf ffmpeg-git-amd64-static.tar.xz
+rm ffmpeg-git-amd64-static.tar.xz
+mv ffmpeg-git-*-amd64-static ffmpeg
+popd
+sls deploy
